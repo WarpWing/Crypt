2510edeb4ce97283a4440cc60e20e37c121a6301
Ty Chermsirivatana <professornitro@gmail.com>
2020-07-16 10:10:39 -0400

    Deleted sleep 1s and echo input

--- git.sh
+++ git.sh
@@ -1,6 +1,5 @@
 echo "Please type the commit message: "
-read INPUT
-echo $INPUT is the commit message :D && sleep 1s   
+read INPUT   
 git add .
 git commit -m "$INPUT"
 git push -u origin master 
